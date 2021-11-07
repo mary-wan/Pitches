d538381943397b0ab34a498986d136233ee3b66e
mary-wan <mary.njenga@student.moringaschool.com>
2021-11-08 01:06:20 +0300

    deploy to heroku

--- manage.py
+++ manage.py
@@ -8,7 +8,7 @@ from app.models import User
 app = create_app('development')
 app = create_app('production')
 # app = create_app('test')
-# app = create_app('production')
+
 
 manager = Manager(app)
 manager.add_command('server',Server)
