403cfceb24e416fc339ce51f69ccfc15df4cff24
mary-wan <mary.njenga@student.moringaschool.com>
2021-11-08 01:54:39 +0300

    deploy

--- manage.py
+++ manage.py
@@ -6,7 +6,7 @@ from app.models import User,Pitch,Upvote,Downvote,Comment
 
 # Creating app instance
 app = create_app('development')
-app = create_app('production')
+# app = create_app('production')
 # app = create_app('test')
 
 
