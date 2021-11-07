09e718e46d6b2fa15a53dae81eee3b9c51ade8d4
mary-wan <mary.njenga@student.moringaschool.com>
2021-11-07 10:09:46 +0300

    add validation for sign up and login

--- app/main/views.py
+++ app/main/views.py
@@ -1,6 +1,14 @@
 from flask import render_template,request,redirect,url_for,abort
 from . import main
+from flask_login import login_required,current_user
+from ..models import User,Pitch,Comment,Upvote,Downvote
+from .forms import UpdateProfile,PitchForm,CommentForm
+from .. import db,photos
 
 @main.route('/')
 def index():
-    return render_template('index.html')
+    pitches = Pitch.query.all()
+    job = Pitch.query.filter_by(category = 'Job').all() 
+    event = Pitch.query.filter_by(category = 'Events').all()
+    advertisement = Pitch.query.filter_by(category = 'Advertisement').all()
+    return render_template('index.html', job = job,event = event, pitches = pitches,advertisement= advertisement)
