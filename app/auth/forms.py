9a54d1c65373ed4fba313a4612ce53f62d0bf7e4
mary-wan <mary.njenga@student.moringaschool.com>
2021-11-07 15:02:08 +0300

    add fontawesome icons

--- app/auth/forms.py
+++ app/auth/forms.py
@@ -1,27 +1,27 @@
 from flask_wtf import FlaskForm
-from wtforms import StringField,PasswordField,SubmitField,ValidationError,BooleanField,TextAreaField
+from wtforms import StringField,PasswordField,SubmitField,ValidationError,BooleanField
 from wtforms.validators import Required,Email,EqualTo
 from ..models import User
 
 class RegistrationForm(FlaskForm):
-    email = StringField('Your Email Address',validators=[Required(),Email()])
-    username = StringField('Enter your username',validators = [Required()])
+    email = StringField('Email',validators=[Required(),Email()])
+    username = StringField('Username',validators = [Required()])
     password = PasswordField('Password',validators = [Required(), EqualTo('password_confirm',message = 'Passwords must match')])
     password_confirm = PasswordField('Confirm Password',validators = [Required()])
-    submit = SubmitField('Sign Up')
+    submit = SubmitField('Register')
     
     def validate_email(self,data_field):
             if User.query.filter_by(email =data_field.data).first():
-                raise ValidationError('There is an account with that email')
+                raise ValidationError('There is an account with that email!!')
 
     def validate_username(self,data_field):
         if User.query.filter_by(username = data_field.data).first():
-            raise ValidationError('That username is taken')
+            raise ValidationError('That username is taken!!')
         
 class LoginForm(FlaskForm):
-    email = StringField('Your Email Address',validators=[Required(),Email()])
+    email = StringField('Email',validators=[Required(),Email()])
     password = PasswordField('Password',validators =[Required()])
     remember = BooleanField('Remember me')
-    submit = SubmitField('Sign In')
+    submit = SubmitField('Log In')
     
 
